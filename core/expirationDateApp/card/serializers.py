2bdcd7c7f2633023ffb8ac234ffab98c3c748f82
bussola <vbussola@yahoo.com>
2019-08-07 16:57:00 -0300

    docker

--- core/expirationDateApp/card/serializers.py
+++ core/expirationDateApp/card/serializers.py
@@ -1,20 +1,16 @@
-from rest_framework.serializers import ModelSerializer
 from expirationDateApp.card.models import Card
-
 from rest_framework.fields import SerializerMethodField
-from rest_framework.serializers import ModelSerializer, Serializer, DecimalField, CharField, DateField, IntegerField
-from rest_framework.exceptions import ValidationError
+from rest_framework.serializers import ModelSerializer
 
 
 class CardSerializer(ModelSerializer):
     card_number = SerializerMethodField()
 
     def get_card_number(self, obj):
-        envrypt_card_number = obj.card_number
-        envrypt_card_number = envrypt_card_number[-4:].rjust(len(envrypt_card_number), "*")
-        return envrypt_card_number
+        encrypt_card_number = obj.card_number
+        encrypt_card_number = encrypt_card_number[-4:].rjust(len(encrypt_card_number), "*")
+        return encrypt_card_number
 
     class Meta:
-
         model = Card
         fields = ('client_id', 'card_holder', 'card_number', 'month', 'year', 'is_active')
