8c5eb21423206147ff641987612dce8a0690bbc9
bussola <vbussola@yahoo.com>
2019-08-08 16:28:17 -0300

    Inserindo primeiros testes

--- core/expirationDateApp/card/tests.py
+++ core/expirationDateApp/card/tests.py
@@ -1,37 +1,53 @@
 from django.test import TestCase
 from expirationDateApp.card.models import Card
+from expirationDateApp.card.serializers import CardSerializer
+from expirationDateApp.client import viewsets
 from expirationDateApp.client.models import Client
 from mixer.backend.django import mixer
 from django.urls import reverse
-from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from expirationDateApp.client.serializers import ClientSerializer
 
 
-class CardTestCase(TestCase):
+class ClientsTest(TestCase):
+
     def setUp(self):
+        self.uri = '/clients/'
+        self.client = APIClient()
         Client.objects.create(
-            name="Joao Jose Silva",
-            cep="15091220",
-            uf="SP",
-            city="Ribeirao Preto",
-            neighborhood="Jd Italia",
-            address="Rua das avenidas",
-            number="23"
-        )
-        cliente = Client.objects.get(name="Joao Jose Silva")
+            name='Joao1', number=1, address='Rua 1', neighborhood='Bairro 1', city='Cidade 1', uf='SP', cep='11999888')
+        Client.objects.create(
+            name='Joao2', number=2, address='Rua 2', neighborhood='Bairro 2', city='Cidade 2', uf='SP', cep='11999887')
+        Client.objects.create(
+            name='Joao3', number=3, address='Rua 3', neighborhood='Bairro 3', city='Cidade 3', uf='SP', cep='11999886')
+        Client.objects.create(
+            name='Joao4', number=4, address='Rua 4', neighborhood='Bairro 4', city='Cidade 4', uf='SP', cep='11999885')
 
-        Card.objects.create(
-            client_id=cliente,
-            card_holder="Joao J Silva",
-            card_number=1234567890123456,
-            month="12",
-            year="2011",
-            is_active=True
-        )
+    def test_get_all_clients(self):
+        # get API response
+        response = self.client.get(self.uri)
+        # get data from db
+        cliente = Client.objects.all()
+        serializer = ClientSerializer(cliente, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, HTTP_200_OK)
 
-        cartoes = mixer.cycle(5).blend('expirationDateApp.Card')
-        print(cartoes)
+    def test_get_valid_single_puppy(self):
+        response = self.client.get(self.uri)
+        cliente = Client.objects.get(pk=1)
+        serializer = ClientSerializer(cliente)
+        self.assertEqual(response.data[0], serializer.data)
+        self.assertEqual(response.status_code, HTTP_200_OK)
 
-    def test_card_is_active(self):
-        card = Card.objects.get(card_number=1234567890123456)
-        self.assertEqual(card.is_active, True)
-
+    def test_get_invalid_single_puppy(self):
+        response = self.client.get(self.uri)
+        try:
+            existe = response.data[10]
+        except Exception as e:
+            existe = None
+            print(e)
+        # print("Negocio: " + str(naoexiste))
+        self.assertEqual(existe, None)
