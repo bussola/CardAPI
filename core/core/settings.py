fc48a47ca16ebd4c1c731de3abfdd0cadc70964c
bussola <vbussola@yahoo.com>
2019-08-08 16:56:39 -0300

    DB on memory

--- core/core/settings.py
+++ core/core/settings.py
@@ -82,6 +82,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': ':memory:',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
